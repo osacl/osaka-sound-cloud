7ef22b16d1093db39078fd28612d4088db2d79e0
leejinjl26-ai <leejin.jl26@gmail.com>
2025-11-03 17:48:05 +0900

    Initial commit

---
+++ src/main/java/com/osaka/osakasoundcloud/OsakasoundcloudApplication.java
@@ -0,0 +1,13 @@
+package com.osaka.osakasoundcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsakasoundcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsakasoundcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osaka/osakasoundcloud/OsakasoundcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osaka.osakasoundcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsakasoundcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
